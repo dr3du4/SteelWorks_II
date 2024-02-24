9a766820bbdcb58a58c8860f9c317f0179afc8e4
dr3du4 <jan.krzyszkowski2@gmail.com>
2024-02-24 20:02:05 +0100

    update afryka3

--- Kobaltowa Przygoda/Assets/Scripts/backpack.cs
+++ Kobaltowa Przygoda/Assets/Scripts/backpack.cs
@@ -1,10 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class backpack : MonoBehaviour
 {
     public float money;
     public int kobalAmount;
-    
+    public GameObject shop;
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (shop.active)
+            {
+                shop.SetActive(false);
+                    
+            }
+            else
+            {
+                shop.SetActive(true);
+            }
+        }
+    }
 }
