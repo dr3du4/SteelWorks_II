19c729dea5a6763933ac65c804c3c094d7076ce4
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-02-25 02:37:07 +0100

    Cobalt Tutorials: Episode II - Attack of the Coroutines

--- Kobaltowa Przygoda/Assets/Scripts/Tutorial/TutorialSystem.cs
+++ Kobaltowa Przygoda/Assets/Scripts/Tutorial/TutorialSystem.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TutorialSystem : MonoBehaviour
 {
     public static TutorialSystem Instance { get; private set; }
+    [SerializeField] private GameObject tutorialDisplay;
+    [SerializeField] private Image tutorialBackground;
+    [SerializeField] private Image titleBackground;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private Image describeBackground;
+    [SerializeField] private TMP_Text describeText;
+    
     [SerializeField] private List<TutorialObject> tutorials = new();
     [SerializeField] private List<bool> wasDisplayed;
 
@@ -21,16 +30,40 @@ public class TutorialSystem : MonoBehaviour
 
 		wasDisplayed = new List<bool>();
 		for(int i=0;i<tutorials.Count;i++) wasDisplayed.Add(false);
+        ShowHideTutorial(false);
     }
 
 
-    public bool DisplayTutorial(int index) {
-        if (wasDisplayed[index]) return false;
-        
+    public void DisplayTutorial(int index) {
         TutorialObject t = tutorials.Find(a => a.index == index);
+        
         //Display tutorial
+        //SetImage
+        tutorialBackground.sprite = t.background;
+        ShowHideTutorial(true); //with clear texts
+        //SetText
+        titleText.text = t.title;
+        describeText.text = t.text;
         
         wasDisplayed[index] = true;
-        return true;
+    }
+
+    public void ShowHideTutorial(bool show) {
+        int x = 0;
+        if (show) x = 255;
+
+        Color c = tutorialBackground.color;
+        tutorialBackground.color = new Color(c.r, c.g, c.b, x);
+        
+        c = titleBackground.color;
+        titleBackground.color = new Color(c.r, c.g, c.b, x);
+        
+        c = describeBackground.color;
+        describeBackground.color = new Color(c.r, c.g, c.b, x);
+
+        titleText.text = "";
+        describeText.text = "";
+
+        tutorialDisplay.SetActive(show);
     }
 }
