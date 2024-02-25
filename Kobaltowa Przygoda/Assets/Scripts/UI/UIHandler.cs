fe70717cfad90f04d573dd264a39abf2b3772463
Bodzio-2 <dungeonraiderpl@gmail.com>
2024-02-25 10:46:20 +0100

    maine scene stuffs

---
+++ Kobaltowa Przygoda/Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+public class UIHandler : MonoBehaviour
+{
+    [Title("UI Binds")]
+    [SerializeField] KeyCode shopToggle = KeyCode.Tab;
+
+    [Space(5)]
+    [Title("UI Elements")]
+    [SerializeField] [SceneObjectsOnly] Canvas shopCanvas;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(shopToggle))
+            ToggleShop();
+
+    }
+
+    void ToggleShop()
+    {
+        if (shopCanvas.gameObject.activeInHierarchy)
+            shopCanvas.gameObject.SetActive(false);
+        else
+            shopCanvas.gameObject.SetActive(true);
+    }
+}
