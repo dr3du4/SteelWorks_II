fb6de20ca1b8e95994b2bb5601847414ec1419ee
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-02-25 16:49:52 +0100

    Player running

--- Kobaltowa Przygoda/Assets/Scripts/Player/playerMovement.cs
+++ Kobaltowa Przygoda/Assets/Scripts/Player/playerMovement.cs
@@ -10,10 +10,15 @@ public class playerMovement : MonoBehaviour
 
     [HideInInspector] public float playerLookAngle;
 
+    private Animator _animator;
     private Rigidbody2D rb;
+    private SpriteRenderer _renderer;
+    private Vector3 previousPosition;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -23,6 +28,7 @@ public class playerMovement : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
         LookTowards(mousePos);
+        CheckMovementDirection();
     }
 
     void CalculateMovement()
@@ -30,8 +36,9 @@ public class playerMovement : MonoBehaviour
         rb.velocity = Vector3.zero;
         float horizontalInput = Input.GetAxisRaw("Horizontal") * speed;
         float verticalInput = Input.GetAxisRaw("Vertical") * speed;
-
         rb.velocity = Vector2.ClampMagnitude(new Vector2(horizontalInput, verticalInput), speed);
+        if (rb.velocity != Vector2.zero) _animator.SetBool("Running", true);
+        else  _animator.SetBool("Running", false);
     }
 
 
@@ -44,5 +51,26 @@ public class playerMovement : MonoBehaviour
         // Rotate Object
         //transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
         playerLookAngle = AngleDeg;
+    }
+    
+    private void CheckMovementDirection()
+    {
+        // Aktualna pozycja obiektu
+        Vector3 currentPosition = transform.position;
+
+        // Różnica między aktualną a poprzednią pozycją
+        Vector3 movement = currentPosition - previousPosition;
+
+        // Sprawdzenie kierunku na podstawie składowej x
+        if (movement.x > 0.1f) {
+            _renderer.flipX = true;
+        }
+        else if (movement.x < -0.1f)
+        {
+            _renderer.flipX = false;
+        }
+
+        // Aktualizacja poprzedniej pozycji
+        previousPosition = currentPosition;
     }
 }
