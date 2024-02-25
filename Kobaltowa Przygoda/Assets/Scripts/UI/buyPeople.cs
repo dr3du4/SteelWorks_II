456978b0d8bf560b1df0d9088de1b9befbc46f74
dr3du4 <jan.krzyszkowski2@gmail.com>
2024-02-25 10:57:28 +0100

    buying people

---
+++ Kobaltowa Przygoda/Assets/Scripts/UI/buyPeople.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buyPeople : MonoBehaviour
+{
+    public int race;
+    public KidsMaster player;
+    
+    public void buySlave()
+    {
+        player.SpawnKids(race);
+    }
+}
